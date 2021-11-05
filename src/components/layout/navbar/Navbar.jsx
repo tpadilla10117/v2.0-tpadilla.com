f7f12f46792cf151b33e60efc3abad2ff9b505b3
Trinidad Padilla <tpadilla101@live.com>
2021-11-05 15:14:40 -0700

    need to fix positioning in the nav bar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -15,11 +15,12 @@ const Navbar = (props) => {
 /* TODO: This is going to be for when you click on the hamburger icon */
     return (
         <nav className="navbar">
+            <div className="nav-items-container">
 
             {/* Logo: */}
 
 
-            {/* {NavbarData.map( (item, index) => {
+            {NavbarData.map( (item, index) => {
                                 return (
                                     <Link to={item.path} className="main-nav-item" style={ {textDecoration: "none"} } key={index}>
                                         <li key={index} className={item.title}>
@@ -27,12 +28,14 @@ const Navbar = (props) => {
                                         </li>
                                     </Link>
                                 )
-                            })} */}
-
-            <img className="mobile-icon" src={NavIcon} alt="" onClick={navToggle}/>
+                            })}
+            </div>
+            <div className="mobile-icon-container">
+                <img className="mobile-icon" src={NavIcon} alt="" onClick={navToggle}/>
+            </div>
             
             {/* <img className="nav-logo" src={Placeholder} alt="" /> */}
-
+                        
 
         </nav>
     );
