87be681b4abc7c0d633d204386696dfdb769b344
Trinidad Padilla <tpadilla101@live.com>
2021-11-05 18:26:56 -0700

    began styling the AboutMe.jsx component

--- src/components/pages/about/AboutMe.jsx
+++ src/components/pages/about/AboutMe.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import './AboutMe.scss';
+import Trin from '../../../assets/images/trin.jpg';
 
 const AboutMe = (props) => {
     return (
-        <div>
-<h1>Greetings from AboutMe</h1>
+        <div id="mainPage-wrapper">
+            <div className="bio-wrapper">
+
+            {/* THIS IS FOR THE IMAGE OF ME*/}
+                <div className="overlay-practice">
+                    <div className="img-box">
+                        <div className="img-container">
+                            <img 
+                                src={Trin} 
+                                className="about-picture" 
+                                alt="Trin Padilla" 
+                                loading="lazy" 
+                                id="leftiimage">
+
+                            </img>
+
+                        </div>
+                        <div className="img-content">
+                            <div>
+                                <p>Hey There! I'm Trin!</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="sideBio">
+
+                </div>
+
+
+            </div>
+
         </div>
     );
 };
