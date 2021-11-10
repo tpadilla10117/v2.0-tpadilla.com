7f19e0e75cb529a6d07041e8f50cc3fc7a1440a6
Trinidad Padilla <tpadilla101@live.com>
2021-11-09 16:34:46 -0800

    finished email logic in form button

--- src/components/layout/emailform/EmailForm.jsx
+++ src/components/layout/emailform/EmailForm.jsx
@@ -8,27 +8,36 @@ const EmailForm = () => {
 /* Logic for Handling Form state & values: */
     const formRef = useRef();
     const [ emailSent, setEmailSent ] = useState(false);
+    const [ buttonText, setButtonText ] = useState("Submit")
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        /* TODO: Review emailjs logic */
+    /* emailjs logic: */
+        emailjs.sendForm('service_2r2pxvp', 'template_1u4c6dc', formRef.current, 'user_ssQeZXemBZwlrNSJZ8wgx')
+      .then((result) => {
+          console.log(result.text);
+          setEmailSent(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      setButtonText("Thank you for sending a message!  I'll get back to you ASAP!");
 
     }
 
     return (
         <div>
             <form ref={formRef} onSubmit={handleSubmit}>
-                <input  type="text" placeholder="Name" name="user_name" />
+                <input type="text" placeholder="Name" name="user_name" />
                 <input type="text" placeholder="Subject" name="user_subject" />
                 <input type="text" placeholder="Email" name="user_email" />
                 <textarea rows="5" placeholder="Message" name="message" /* style={ {backgroundColor: darkMode && "#333"}} *//>
-
-                {/* TODO: Maybe put a mail icon? */}
+        
                 <button className="contact-button">
-                    <img className="formBtn-img" src={paperPlane} alt=""/>Submit
+                    <img className="formBtn-img" src={paperPlane} alt=""/>{!emailSent ? "Submit" : emailSent && buttonText}
                 </button>
-                {emailSent && "Thank you for sending a message!  I'll get back to you ASAP!"}
+                
             </form>
         
         </div>
