2c5f2f8bf2ab42c6375ce14f59d0fb5ea41897b8
Trinidad Padilla <tpadilla101@live.com>
2021-12-11 11:19:53 -0800

    got basic styling done for footer.jsx

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import { FaTwitter, FaLinkedin, FaBehance, FaGithub } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './Footer.scss';
 
@@ -10,12 +10,34 @@ function  Footer() {
                 <div className='footerlinks-container'>
                     <div className='footerlinks-wrapper'>
                         <div className='footerlink-items'>
-                            <ul className='footerlink-title'>
-                                <Link className='footerlink' to="/">
-                                    Home
-                                </Link>
-                            </ul>
-
+                            {/* <ul className='footerlink-title'>Home</ul> */}
+                                <Link className='footerlink' to="/about">
+                                    About
+                                </Link>                            
+                        </div>
+                        <div className='footerlink-items'>
+                            {/* <ul className='footerlink-title'>Home</ul> */}
+                                <Link className='footerlink' to="/developer">
+                                    Developer
+                                </Link>                            
+                        </div>
+                        <div className='footerlink-items'>
+                            {/* <ul className='footerlink-title'>Home</ul> */}
+                                <Link className='footerlink' to="/ux">
+                                    UX
+                                </Link>                            
+                        </div>
+                        <div className='footerlink-items'>
+                            {/* <ul className='footerlink-title'>Home</ul> */}
+                                <Link className='footerlink' to="/contact">
+                                    Contact
+                                </Link>                            
+                        </div>
+                        <div className='footerlink-items'>
+                            {/* <ul className='footerlink-title'>Home</ul> */}
+                                <Link className='footerlink' to="/resume">
+                                    Resume
+                                </Link>                            
                         </div>
 
                     </div>
@@ -24,15 +46,24 @@ function  Footer() {
 
                 <section className='socialmedia'>
                     <div className='socialmedia-wrap'>
-                        <Link className='sociallogo'>
-
+                        <Link className='sociallogo' to='/'>
+                            Trin Padilla
                         </Link>
                         <small className='websiterights'>
-
+                            Trin Padilla © {new Date().getFullYear() } All rights reserved.
                         </small>
                         <div className='socialicons'>
                             <a className='socialiconlink' href='/' target="blank" aria-label="Linkedin">
                                 <FaLinkedin />
+                            </a>
+                            <a className='socialiconlink' href='/' target="blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </a>
+                            <a className='socialiconlink' href='/' target="blank" aria-label="Github">
+                                <FaGithub />
+                            </a>
+                            <a className='socialiconlink' href='/' target="blank" aria-label="Behance">
+                                <FaBehance />
                             </a>
 
                         </div>
