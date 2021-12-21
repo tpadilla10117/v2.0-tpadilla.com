8aa73a3a158ece7d81c9c3775793ea14774037af
Trinidad Padilla <tpadilla101@live.com>
2021-12-21 10:06:34 -0800

    polishing the developer.jsx

--- src/components/layout/uxHeroSection/UxHeroSection.jsx
+++ src/components/layout/uxHeroSection/UxHeroSection.jsx
@@ -5,13 +5,13 @@ import uxbeach from '../../../assets/images/uxbeach.jpg';
 
 const UxHeroSection = () => {
     return (
-        <section id="video-container">
-            <div className='videoSection-bg'>
-                <img className='videobg' src={uxbeach} alt='Cabanas along a beach' /* autoPlay loop muted src={water} type="video/mp4" *//>
+        <section id="ux-hero-container">
+            <div className='ux-heroSection-bg'>
+                <img className='ux-hero-img' src={uxbeach} alt='Cabanas along a beach' /* autoPlay loop muted src={water} type="video/mp4" *//>
             </div>
-            <div className='videocontent'>
-                <h1 className='hero-h1'>Developer</h1>
-                <p className='hero-p'>Here is a collection of some of my web-development work:</p>
+            <div className='ux-hero-content'>
+                <h1 className='ux-hero-h1'>Developer</h1>
+                <p className='ux-hero-p'>Here is a collection of some of my web-development work:</p>
             </div>
             
         </section>
