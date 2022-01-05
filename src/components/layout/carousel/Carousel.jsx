db1aacc3e40baffa4b9cef7eec0a133c48770943
Trinidad Padilla <tpadilla101@live.com>
2022-01-05 14:10:19 -0800

    got the arrows styled - had to use the actual svg in the react component

--- src/components/layout/carousel/Carousel.jsx
+++ src/components/layout/carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, /* useEffect */ } from 'react';
 import { /* carouselData, */ carouselIndicatorData } from '../../../utils/seed';
-import chevronRight from '../../../assets/icons/chevron-right.svg';
-import chevronLeft from '../../../assets/icons/chevron-left.svg';
+/* import chevronRight from '../../../assets/icons/chevron-right.svg'; */
+/* import chevronLeft from '../../../assets/icons/chevron-left.svg'; */
 import './Carousel.scss';
 import { PersonalCards } from '../../utils';
 
@@ -68,9 +68,20 @@ console.log(slides)
                 <div className='arrow-container'>
 
                     {/* Right Arrow : */}
-                    <img className="right-arrow" src={chevronRight} alt="" onClick={nextSlide}/>
+                    {/* <img className="right-arrow" src={chevronRight} alt="" onClick={nextSlide}/> */}
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="right-arrow" viewBox="0 0 20 20" fill="#fff" height='70' width="50" onClick={nextSlide} >
+                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+
+                    </svg>
+                    
                     {/* Left Arrow: */}
-                    <img className="left-arrow" src={chevronLeft} alt="" onClick={previousSlide}/>
+                    {/* <img className="left-arrow" src={chevronLeft} alt="" onClick={previousSlide}/> */}
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="left-arrow" viewBox="0 0 20 20" fill="#fff" height="70" width="50" onClick={previousSlide}>
+                        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                    </svg>
+
                 </div>
 
                     {/* carouselData */cardInfo.map( (slide, index) => {
