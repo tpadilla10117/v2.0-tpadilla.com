0ddd7edfa9fc0c6842f2d3e0b489869e2517301f
Trinidad Padilla <tpadilla101@live.com>
2022-01-26 15:21:59 -0800

    cheah

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, {useEffect} from 'react';
+import React/* , {useEffect} */ from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { useLocation } from "react-router-dom";
+/* import { useLocation } from "react-router-dom"; */
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-function ScrollToTop() {
+/* function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -14,7 +14,7 @@ function ScrollToTop() {
   }, [pathname]);
 
   return null;
-}
+} */
 
 ReactDOM.render(
   <React.StrictMode>
